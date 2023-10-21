b7aec84fd09d4d49628c4f01fbf95ac075497372
theindianwizard <93317473+theindianwizard@users.noreply.github.com>
2023-10-21 12:05:38 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyB6-QUcHDTb-QRfbSnKrVgmo0Ht-nVrESM",
+    authDomain: "shiva-70126.firebaseapp.com",
+    projectId: "shiva-70126",
+    storageBucket: "shiva-70126.appspot.com",
+    messagingSenderId: "802693051290",
+    appId: "1:802693051290:web:57411e301fc0d9d3378023"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
